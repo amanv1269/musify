f952e8c9c20b4a8acbd470444cb85ec7748b3996
amanv1269 <168532444+amanv1269@users.noreply.github.com>
2024-06-09 17:57:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,20 @@ app.use(express.json());
 const userRoute = require("./routes/userRoute");
 const songsRoute = require("./routes/songsRoute");
 const adminRoute = require("./routes/adminRoute");
-app.get("/", (req, res) => {
-  app.use(express.static(path.resolve(__dirname, "frontend", "build")));
-  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-});
+
+// Serve static files before defining other routes
+app.use(express.static(path.resolve(__dirname, "frontend", "build")));
+
+// Define your API routes
 app.use("/api/users", userRoute);
 app.use("/api/songs", songsRoute);
 app.use("/api/admin", adminRoute);
-const port = 5000;
-app.listen(port, () => console.log(`node js server started at port ${port}!`));
+
+// Define route handler for the root path ("/")
+app.get("/", (req, res) => {
+  // Send the index.html file from the build folder
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Node.js server started at port ${port}!`));
